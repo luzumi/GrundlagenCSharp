68c1e08bd3b7513d9b180fbcf5d84173e1a320d8
Daniel Neubieser <corataca@outlook.de>
2020-11-02 09:26:47 +0100

    Projektdateien hinzufügen.

---
+++ Kontrollstrukturen/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+//Solution GrundlagenCSharp
+// .NetCore Application 'Kontrollstrukturen' erstellt
+//Haken bei Solution im Projektverzeichnis rausgenommen, alle weiteren Projekte werden dann Teil der Solution
+namespace Kontrollstrukturen
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
