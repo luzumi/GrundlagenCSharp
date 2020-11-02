4395fac76ff48bc23a453131153dc2cce9200d3c
Daniel Neubieser <corataca@outlook.de>
2020-11-02 10:50:48 +0100

    Kommentare erklärt

--- Kontrollstrukturen/Program.cs
+++ Kontrollstrukturen/Program.cs
@@ -1,11 +1,26 @@
-﻿using System;
-//Solution GrundlagenCSharp
+﻿//Solution GrundlagenCSharp
 // .NetCore Application 'Kontrollstrukturen' erstellt
 //Haken bei Solution im Projektverzeichnis rausgenommen, alle weiteren Projekte werden dann Teil der Solution
+
+using System;
+// normaler kommetar
+/*
+ * mehrzeiliger kommentar
+ * oder Kommentar der mitten in eine Zeile geschrieben werden kann
+ */
+
 namespace Kontrollstrukturen
 {
+/// <summary>
+/// Kommentar der zu jeder Funktion eine Beschreibung mitleifert, der dann auch direkt angezeigt wird,
+/// wenn die Maus über den Funktionsnamen gehalten wird
+/// </summary>
     class Program
     {
+        /// <summary>
+        /// Diese Funktion startet das Programm
+        /// </summary>
+        /// <param name="args">Kommandozeilenparameter</param>
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
